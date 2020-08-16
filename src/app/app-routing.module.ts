71400fb2c548e98a67473ebd399664f47bc1d883
rreyes01 <rreyes@smartdream.es>
2020-08-16 01:50:07 -0400

    Cambio de ruta por defecto al login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { LoginComponent } from './auth/login/login.component';
 const routes: Routes = [
     {
       path: '',
-      redirectTo: '/home',
+      redirectTo: '/login',
       pathMatch: 'full',
     },
     // {
